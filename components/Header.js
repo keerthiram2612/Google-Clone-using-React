d3975ef844441487ed4be2c841df86d4ca87a66c
keerthiram2612 <keerthiram2612@gmail.com>
2022-12-31 21:04:49 +0530

    create body section

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
 // @flow strict
+
 import User from "./User";
-
-
+import {FcAbout} from "react-icons/Fc"
+import {FaStoreAlt} from "react-icons/fa"
 export default function Header(){
     return(
         <header className="flex justify-between p-5 text-sm  text-gray-700">
             <div className="flex space-x-4 items-center">
-            <p className="link">About</p>
-            <p className="link">Store</p>
+           <FcAbout className="w-8 h-8 link " />
+            <FaStoreAlt className="w-8 h-8 link " />
             </div>
             <div className="flex space-x-4 p-5 item-center">
                 <p className="link pt-5 px-2">Gmail</p>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
 import Header from"../components/Header";
-
-
+import{BiSearchAlt} from "react-icons/Bi";
+import{FaMicrophone} from "react-icons/Fa"
 export default function Home() {
   return (
     <>
@@ -11,9 +12,28 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* Header*/}
       
       <Header/>
-    
-    </>
-  )
+
+      {/*Body*/}
+
+      <form className="flex flex-col items-center mt-40">
+        <Image width="300"
+        object-Fit="cover"
+         height="100" 
+         src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"/>
+         <div className="flex w-full mt-5 mx-auto mx-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-x lg:max-w-2xl">
+         <BiSearchAlt className=" w-8 h-8 text-gray-500 mr-3" />
+         <input type="text" className="flex-grow focus:outline-none"/>
+         <FaMicrophone className="w-8 h-8"/>
+         </div>
+         <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+         <button className="btn">Google Search</button>
+         <button className="btn">I'm Feeling bliss</button>
+         </div>
+         </form>
+      {/*Footer*/}
+      </>
+  );
 }
