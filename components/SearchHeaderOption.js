4bafb6e9d1b79fa59604fd948060baae7483ae61
keerthiram2612 <keerthiram2612@gmail.com>
2023-01-15 12:09:24 +0530

    after the error of the create search header options

--- components/SearchHeaderOption.js
+++ components/SearchHeaderOption.js
@@ -1,16 +1,21 @@
+import { useRouter } from "next/router";
 
-import {useRouter} from "next/router";
-
-export default function SearchHeaderOption({title,selected}){
-    const router = useRouter ()
-    function selectTab(title){
-        router.push(`/search?term=${router.query.term}&searchType=${title === "images " ? "image" : "" }`
+export default function SearchHeaderOption({ title, Icon, selected }) {
+    const router = useRouter();
+    function selectTab(title) {
+        router.push(
+            `/search?term=${router.query.term}&searchType=${title === "Images" ? "image" : ""
+            }`
         );
-}
-    return(
-        <div onclick={()=>selectTab(title)}
-        className={`flex-items-center space-x-1 border-b-4 border-transparent hover:text-blue-500 cursor-pointer hover:border-blue-500 pb-3 ${selected && "text-blue-500 border-blue-500"}`}>
+    }
+    return (
+        <div
+            onClick={() => selectTab(title)}
+            className={`flex items-center space-x-1 border-b-4 border-transparent hover:text-blue-500 cursor-pointer hover:border-blue-500 pb-3 ${selected && "text-blue-500 border-blue-500"
+                }`}
+        >
+            <Icon className="h-4" />
             <p>{title}</p>
         </div>
-    )
+    );
 }
