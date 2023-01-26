071465290ead4ba8f9674a238e516aeaac103252
keerthiram2612 <keerthiram2612@gmail.com>
2023-01-26 09:46:49 +0530

    create the about data section and modify the title in search page

--- pages/search.js
+++ pages/search.js
@@ -2,17 +2,21 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
 import Response from "../Response";
+import SearchResults from "../components/SearchResults";
+import {useRouter} from "next/router";
 export default function search({results}){
   console.log(results);
+  const router=useRouter()
         return (
             <div>
                 <Head>
-                  <title>Search Page</title>
+                  <title>{router.query.term} - Search page</title>
                 </Head>
 
                 {/*Search Header*/}
                    <SearchHeader/>
                 {/*Search Results*/}
+                <SearchResults results={results}/>
             </div>
         );
         }
