633ca1279c07e42a8f5d25aa99979d6a08150f3d
keerthiram2612 <keerthiram2612@gmail.com>
2023-01-26 23:00:13 +0530

    add image results component

--- pages/search.js
+++ pages/search.js
@@ -4,6 +4,7 @@ import SearchHeader from "../components/SearchHeader";
 import Response from "../Response";
 import SearchResults from "../components/SearchResults";
 import {useRouter} from "next/router";
+import ImageResults from "../components/imageResults";
 export default function search({results}){
   console.log(results);
   const router=useRouter()
@@ -15,15 +16,19 @@ export default function search({results}){
 
                 {/*Search Header*/}
                    <SearchHeader/>
-                {/*Search Results*/}
+                {/*Search web and Images Results*/}
+                {router.query.searchType === "image"?(
+                  <ImageResults results={results}/>
+                ) :(
                 <SearchResults results={results}/>
+                )}
             </div>
         );
         }
 
         export async function getServerSideProps(context){
           const startIndex = context.query.start || "1"
-          const mockData = false
+          const mockData = false;
           const data = mockData ? Response : await fetch(
             ` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image" }&start=${startIndex}`
           ).then((response)=>response.json()) 
