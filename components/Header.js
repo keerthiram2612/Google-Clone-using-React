23af5bd45763d6319ca5fd51d42120056b233a28
keerthiram2612 <keerthiram2612@gmail.com>
2023-01-28 22:43:10 +0530

    upated icons

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,21 @@
-// @flow strict
-
 import User from "./User";
-import Link from"next/link"
-import {FcAbout} from "react-icons/Fc"
-import {FaStoreAlt} from "react-icons/fa"
-import { useRouter } from "next/router";
-export default function Header(){
-    const router=useRouter();
-    return(
-        <header className="flex justify-between p-5 text-sm  text-gray-700">
-            <div className="flex space-x-4 items-center">
-            <Link href="https://about.google/">
-           <FcAbout className="w-8 h-8 link " />
-           </Link>
-           <Link href="https://store.google.com">
-            <FaStoreAlt className="w-8 h-8 link " />
-            </Link>
-            </div>
-            <div className="flex space-x-4 p-5 item-center ">
-                <Link className="link pt-5 px-2 " href="https://mail.google.com/">
-                    Gmail</Link>
+import Link from "next/link"
+import { useRouter } from "next/router"
 
-                <a onClick={()=>router.push(`/search?terms=${router.query.term || "google"}&searchType=image`)}className="link pt-5 px-2">Images</a>
-    
-                <User/>
-            </div>
-        </header>
-    )
+export default function Header() {
+  const router = useRouter();
+  return (
+    <header className="flex justify-between p-5 text-sm text-gray-700flex space-x-4">
+      <div className="flex space-x-4 items-center">
+        <Link className="link" href="https://about.google/">About</Link>
+        <Link className="link" href="https://store.google.com/">Store</Link>
+      </div>
+      <div className="flex space-x-4 items-center">
+        <Link className="link" href="https://mail.google.com"> Gmail</Link>
+
+        <a onClick={() => router.push(`/search?term=${router.query.term || "google"}&searchType=image`)} className="link">Images</a>
+        <User />
+      </div>
+    </header>
+  )
 }
