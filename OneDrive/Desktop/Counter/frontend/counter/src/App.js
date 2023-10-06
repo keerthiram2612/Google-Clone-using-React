22162d2aecb47837b16798a69f725e74d3756858
keerthiram2612 <keerthiram2612@gmail.com>
2023-10-06 20:50:08 +0530

    styling the counter app

--- OneDrive/Desktop/Counter/frontend/counter/src/App.js
+++ OneDrive/Desktop/Counter/frontend/counter/src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <div>
+      <h1>Counter App</h1>
       <Counter/>
     </div>
   );
--- OneDrive/Desktop/Counter/frontend/counter/src/Components/Counter1.jsx
+++ OneDrive/Desktop/Counter/frontend/counter/src/Components/Counter1.jsx
@@ -13,16 +13,18 @@ function Counter() {
   const decrement = () => {
     setCount(count - 1);
   };
-
+  
   return (
     <div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       {/*  Display the counter value */}
       <h1>Counter: {count}</h1>
       {/*  Add buttons to increment and decrement the counter */}
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}><i class="fa-solid fa-plus"></i></button>
+      <button onClick={decrement}><i class="fa-solid fa-minus"></i></button>
     </div>
   );
 }
 
 export default Counter;
+
